a982f01491416ac13ab224e6aba0c56e664ae520
Hoon Wee <mrgravity817@gmail.com>
2023-01-21 01:33:32 +0900

    test: add failing tests

--- tests/test_furnice.py
+++ tests/test_furnice.py
@@ -21,4 +21,22 @@ def test_can_allocate_if_available_greater_than_required():
     # Assert with Act
     assert large_batch.can_allocate(small_line)
 
+def test_cannot_allocate_if_available_smaller_than_required():
+    # Arrange
+    small_batch, large_line = make_batch_and_line("SOFA", 4, 20)
+    # Assert with Act
+    assert small_batch.can_allocate(large_line) is False
+
+def test_can_allocate_if_available_equal_to_required():
+    # Arrange
+    batch, line = make_batch_and_line("SOFA", 20, 20)
+    # Assert with Act
+    assert batch.can_allocate(line)
+
+def test_cannot_allocate_if_skus_do_not_match():
+    # Arrange
+    batch  = Batch("batch-001", "Chair", 100, eta=None)
+    different_sku_line = OrderLine("order-123", "Toaster", 10)
+    # Assert with Act
+    assert batch.can_allocate(different_sku_line) is False
 
