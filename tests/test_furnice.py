eef66d301d3bbd4917345e57899e912000c7b953
Hoon Wee <mrgravity817@gmail.com>
2023-01-21 00:58:03 +0900

    refactor: create factory for creating Batch and OrderLine instance

--- tests/test_furnice.py
+++ tests/test_furnice.py
@@ -1,10 +1,15 @@
 from furnice.model import Batch, OrderLine
 from datetime import date
 
+def make_batch_and_line(sku: str, batch_qty: int, line_qty: int): 
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty)
+    )
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     # Arrange
-    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = OrderLine('order-ref', "SMALL-TABLE",2)
+    (batch, line) = make_batch_and_line("SMALL-TABLE", 20, 2)
     # Act
     batch.allocate(line)
     # Assert
