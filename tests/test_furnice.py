a9a04648daf4e0145711a6d3dd4ec2cb2005109e
Hoon Wee <mrgravity817@gmail.com>
2023-01-21 01:04:34 +0900

    feat: check if can allocate

--- tests/test_furnice.py
+++ tests/test_furnice.py
@@ -9,9 +9,16 @@ def make_batch_and_line(sku: str, batch_qty: int, line_qty: int):
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     # Arrange
-    (batch, line) = make_batch_and_line("SMALL-TABLE", 20, 2)
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
     # Act
     batch.allocate(line)
     # Assert
     assert batch.available_quantity == 18
+
+def test_can_allocate_if_available_greater_than_required():
+    # Arrange
+    large_batch, small_line = make_batch_and_line("SOFA", 20, 4)
+    # Assert with Act
+    assert large_batch.can_allocate(small_line)
+
 
