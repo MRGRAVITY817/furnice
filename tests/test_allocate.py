7571304734e1bb8f1374c2c72a49d2c368f1434b
Hoon Wee <mrgravity817@gmail.com>
2023-01-21 15:01:54 +0900

    test: should prefer current stock batches when allocating order line

---
+++ tests/test_allocate.py
@@ -0,0 +1,17 @@
+from furnice.model import Batch, OrderLine
+from datetime import date, timedelta
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=10)
+
+def test_prefers_current_stock_batches_to_shipments():
+	# Arrange
+	in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+	shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+	line = OrderLine("oref", "RETRO-CLOCK", 10)
+	# Act
+	allocate(line, [in_stock_batch, shipment_batch])
+	# Assert
+	assert in_stock_batch.available_quantity == 90
+	assert shipment_batch.available_quantity == 100
