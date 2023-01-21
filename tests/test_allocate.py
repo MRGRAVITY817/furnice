df76d5f4cfc2df622046acab67c9c0122e1fa5f2
Hoon Wee <mrgravity817@gmail.com>
2023-01-21 15:20:22 +0900

    test: should prefer batch that's available earlier

--- tests/test_allocate.py
+++ tests/test_allocate.py
@@ -6,12 +6,23 @@ tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
 def test_prefers_current_stock_batches_to_shipments():
-	# Arrange
 	in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
 	shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
 	line = OrderLine("oref", "RETRO-CLOCK", 10)
-	# Act
+
 	allocate(line, [in_stock_batch, shipment_batch])
-	# Assert
+
 	assert in_stock_batch.available_quantity == 90
 	assert shipment_batch.available_quantity == 100
+
+def test_prefers_earlier_batches():
+	earliest = Batch("speedy-batch", "MINIMALIST-SPOON", 100, eta=today)
+	medium= Batch("normal-batch", "MINIMALIST-SPOON", 100, eta=tomorrow)
+	latest = Batch("slow-batch", "MINIMALIST-SPOON", 100, eta=later)
+	line = OrderLine("order1",  "MINIMALIST-SPOON", 10)
+
+	allocate(line, [earliest, medium, latest])
+
+	assert earliest.available_quantity == 90
+	assert medium.available_quantity == 100
+	assert latest.available_quantity == 100
